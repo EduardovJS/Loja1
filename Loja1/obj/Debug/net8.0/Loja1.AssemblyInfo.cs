28f6c51777a9b16c43b5d9ee226febab7a82a473
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-10-23 20:33:46 -0300

    adicionando carousel

--- Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
+++ Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4debebb58f77d3f1c0cb02c6390ee1b84e8e520")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
