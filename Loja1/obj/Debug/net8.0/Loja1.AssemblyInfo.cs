411d33e307c3eea1d599247d664e709a21a1ee03
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-10-23 21:21:40 -0300

    migration inicial

--- Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
+++ Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2406d4e7d61b1919825ca837404fba744f5da86c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70f3431ea0b85258cb115f43fbf9e853fb00560")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
