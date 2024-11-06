1eca8489ab9079be73c8623a5a25bd81e2ced0be
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-11-06 05:16:42 -0300

    ajustando lachescontroller para exibir categoria

--- Loja1/Controllers/LanchesController.cs
+++ Loja1/Controllers/LanchesController.cs
@@ -1,4 +1,5 @@
-﻿using Loja1.Repositories.Interfaces;
+﻿using Loja1.Models;
+using Loja1.Repositories.Interfaces;
 using Loja1.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,12 +15,36 @@ namespace Loja1.Controllers
         }
 
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
-            return View(lanchesListViewModel);
+
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal")).OrderBy(l => l.Nome);
+                }
+                else
+                {
+                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Natural")).OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+
+            }
+            var lancheListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual,
+            };
+            return View(lancheListViewModel);
         }
     }
 }
--- Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
+++ Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff70b40ead97eac5c3dcad21e460c23940617c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a068ce6a695eaadb0f8a48a4206f9335114a0e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
