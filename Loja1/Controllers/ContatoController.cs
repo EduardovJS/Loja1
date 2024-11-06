6a068ce6a695eaadb0f8a48a4206f9335114a0e5
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-11-06 05:03:40 -0300

    criando talghelpers

---
+++ Loja1/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loja1.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
+++ Loja1/obj/Debug/net8.0/Loja1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21fc5884bb763a56ef1e10561ab05f754199b06c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff70b40ead97eac5c3dcad21e460c23940617c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
